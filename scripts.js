01e600bd258122fa7c4d989d765db1b06a41f3f4
masy <taurusmasy@yahoo.com.hk>
2021-02-22 23:02:44 +0800

    index-slider

--- scripts.js
+++ scripts.js
@@ -40,3 +40,54 @@ function resizeHandler() {
 menuBtn.addEventListener("click", openNav);
 menuCloseBtn.addEventListener("click", closeNav);
 window.visualViewport.addEventListener('resize', resizeHandler);
+
+// =================================
+// index-slider > slider
+// =================================
+
+const slides = document.getElementsByClassName("slide");
+const dots = document.getElementsByClassName("dot");
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  if (n > slides.length) {slideIndex = 1};
+  if (n < 1) {slideIndex = slides.length};
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < slides.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+carousel();
+
+function carousel() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < slides.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1};
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(carousel, 5000);
+}
+
+
+
