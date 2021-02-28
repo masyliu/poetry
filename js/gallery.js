76226abc565a8f1f153f527e6ec6e57ab5a59219
masy <taurusmasy@yahoo.com.hk>
2021-02-28 21:02:38 +0800

    update gallery overlay function

--- js/gallery.js
+++ js/gallery.js
@@ -8,12 +8,14 @@ const galleryOverlay = document.querySelector('.gallery-overlay');
 // const overlayInner = document.querySelector('.gallery-overlayInner');
 const overlayImage = galleryOverlay.querySelector('img');
 const overlayClose = galleryOverlay.querySelector('.galleryCloseBtn');
+let imageIndex;
 
 function openImage(e) {
+  imageIndex = this.dataset.no-1;
+  console.log(imageIndex);
   const src = e.currentTarget.querySelector('img').src;
   overlayImage.src = src;
   galleryOverlay.classList.add("open");
-  console.log(galleryOverlay.style.display);
 }
 
 function handleClose() {
@@ -21,11 +23,26 @@ function handleClose() {
 }
 
 function handleClose2(e) {
-  if (!e.target.matches('.gallery-overlayInner') && !e.target.matches('img')) {
+  console.log(e.target);
+  if (!e.target.matches('.gallery-overlayInner') && !e.target.matches('img') && !e.target.matches('a') && !e.target.matches('span')) {
     galleryOverlay.classList.remove('open');
   }
+}
+
+function plusImages(n) {
+  imageIndex += n;
+  console.log(imageIndex);
+  if (imageIndex > galleryImages.length) imageIndex = 0;
+  if (imageIndex < 0) imageIndex = galleryImages.length;
+  try {
+    overlayImage.style.opacity = "1"
+    overlayImage.src = galleryImages[imageIndex].querySelector('img').src
+} catch {
+  overlayImage.style.opacity = "0.5";
+};
 }
 
 galleryImages.forEach(image => image.addEventListener('click', openImage));
 overlayClose.addEventListener('click', handleClose);
 galleryOverlay.addEventListener('click', handleClose2);
+
